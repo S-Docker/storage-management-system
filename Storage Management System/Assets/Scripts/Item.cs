5f82909899ee739ca6b2373c3f1e1127f95e9d93
S-Docker <steven.docker@hotmail.co.uk>
2020-03-12 23:55:04 +0000

    Empty project set up
    Setting up empty project

---
+++ Storage Management System/Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Items/Item", fileName = "New Item")]
+public class Item : ScriptableObject {
+    [SerializeField]
+    private int id;
+    [SerializeField]
+    private string itemName;
+    [SerializeField]
+    private string description;
+    [SerializeField]
+    private Sprite icon;
+    [Range(1, 20)]
+    [SerializeField]
+    private int maximumStackSize;
+}
